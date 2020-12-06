1f8aa6aa21a29aac2f1528c27dca2c501352cb7c
gikman <72310618+gikman@users.noreply.github.com>
2020-12-07 00:05:37 +0300

    Add files via upload

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+    return (
+        <div className='container'>
+            <div className="jumbotron mt-5">
+                <h1 className="display-4">Welcome to Blog Life</h1>
+                <p className="lead">We make all kinds of awesome blog about various topics.</p>
+                <hr className="my-4"/>
+                <p>Click to the button below to check out our awesome blog.</p>
+                <Link className="btn btn-primary btn-lg" to="/blog" role="button">Check out our Blog</Link>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Home
